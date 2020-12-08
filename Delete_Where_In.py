c9ef50e3753c42c19dcf536ee72523017e86bee1
Stephen Keogh <stevokeogh@hotmail.com>
2020-12-08 18:07:38 +0000

    finished

---
+++ Delete_Where_In.py
@@ -0,0 +1,25 @@
+import os
+import datetime
+import pymysql
+
+# Get username from Cloud9 workspace
+# (modify this variable if running on another environment)
+username = os.getenv('C9_USER')
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        list_of_names = ['fred','Fred']
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names)
+        connection.commit()
+
+finally:
+    # Close the connection, regardless of whether or not the above was successful
+    connection.close()
